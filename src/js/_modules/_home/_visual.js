48e5e6377e68835b2b2e32b0c5cc77cb5af7cf7e
shossk <work@shossk.com>
2022-09-28 23:11:10 +0900

    fix: fixed scroll animation

--- src/js/_modules/_home/_visual.js
+++ src/js/_modules/_home/_visual.js
@@ -11,35 +11,6 @@ export default class {
    * init
    */
   init() {
-    const tl = gsap.timeline({
-      repeat: -1,
-    })
-
-    tl.from('.js-logo-img', {
-      ease: 'power4.out',
-      duration: 1,
-      xPercent: -100,
-      stagger: 0.15,
-    })
-      .from(
-        '.js-visual-txt',
-        {
-          ease: 'power4.out',
-          duration: 1,
-          yPercent: 100,
-        },
-        '-=1.2'
-      )
-      .from(
-        '.js-nav-txt',
-        {
-          ease: 'power4.out',
-          duration: 1,
-          yPercent: 100,
-        },
-        '-=1.2'
-      )
-
     gsap.registerPlugin(ScrollTrigger)
     const topCompanyWidth = document.querySelector('.top-company__main').clientWidth
     const topCompanyIcon = document.querySelector('.top-company__icon').clientWidth
@@ -49,10 +20,11 @@ export default class {
         trigger: '.top-company',
         start: 'top top',
         end: '+=3000',
-        scrub: true,
+        scrub: 1.5,
         pin: true,
       },
     })
+
     topTl.fromTo('.top-company_icon_logo', { x: (topCompanyWidth - topCompanyIcon) / 2 }, { x: 0, ease: 'Power4.out' })
     topTl.fromTo('.top-company__txt', { x: 100, opacity: 0}, { x: 0, opacity: 1, ease: 'Power4.out' })
     topTl.to('.top-company__txt', { x: -100, opacity: 0, delay: 1.2 })
